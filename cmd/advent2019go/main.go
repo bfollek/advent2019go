042094686acd569f5c30aff6ebd0ff209f6f6346
Bob Follek <bfollek@gmail.com>
2019-12-04 21:43:33 -0500

    Started day03, added day01 to main

--- cmd/advent2019go/main.go
+++ cmd/advent2019go/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 
+	"github.com/bfollek/advent2019go/day01"
 	"github.com/bfollek/advent2019go/day02"
 )
 
 func main() {
+	fmt.Printf("day01.Part1: %d\n", day01.Part1("../../day01/testdata/day01.dat"))
+	fmt.Printf("day01.Part2: %d\n", day01.Part2("../../day01/testdata/day01.dat"))
+
 	fmt.Printf("day02.Part1: %d\n", day02.Part1("../../day02/testdata/day02.dat"))
 	if i, err := day02.Part2("../../day02/testdata/day02.dat"); err != nil {
 		fmt.Printf("day02.Part2 error: %s\n", err)
