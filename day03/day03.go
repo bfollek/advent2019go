9849cb275b11cc17ca69feec43c95c18e98db045
Bob Follek <bfollek@gmail.com>
2019-12-06 04:30:50 -0500

    BF WIP

--- day03/day03.go
+++ day03/day03.go
@@ -14,7 +14,7 @@ const down = 'D'
 const right = 'R'
 const left = 'L'
 
-type closestFunc func(map[point]int64, map[point]int64) int64
+type closeFunc func(point, map[point]int64, map[point]int64) int64
 
 type move struct {
 	direction byte
@@ -41,30 +41,15 @@ func Part2(fileName string) int64 {
 	return calcClosest(fileName, closestBySteps)
 }
 
-func calcClosest(fileName string, f closestFunc) int64 {
+func calcClosest(fileName string, f closeFunc) int64 {
 	wire1Moves, wire2Moves := loadMoves(fileName)
 	wire1Path := getPath(wire1Moves)
 	wire2Path := getPath(wire2Moves)
-	return f(wire1Path, wire2Path)
-}
-
-func closestByManhattanDistance(wire1Path, wire2Path map[point]int64) int64 {
 	crossPoints := intersection(wire1Path, wire2Path)
 	closest := int64(math.MaxInt64)
 	for _, p := range crossPoints {
-		if md := manhattanDistance(p); md < closest {
-			closest = md
-		}
-	}
-	return closest
-}
-
-func closestBySteps(wire1Path, wire2Path map[point]int64) int64 {
-	crossPoints := intersection(wire1Path, wire2Path)
-	closest := int64(math.MaxInt64)
-	for _, p := range crossPoints {
-		if steps := wire1Path[p] + wire2Path[p]; steps < closest {
-			closest = steps
+		if howClose := f(p, wire1Path, wire2Path); howClose < closest {
+			closest = howClose
 		}
 	}
 	return closest
@@ -72,9 +57,13 @@ func closestBySteps(wire1Path, wire2Path map[point]int64) int64 {
 
 // manhattanDistance formula: |x1 - x2| + |y1 - y2|
 // https://xlinux.nist.gov/dads/HTML/manhattanDistance.html
-func manhattanDistance(p point) int64 {
+func closestByManhattanDistance(p point, _, _ map[point]int64) int64 {
 	// The second point is always centralPort
 	return util.AbsInt64(p.x-centralPort.x) + util.AbsInt64(p.y-centralPort.y)
+}
+
+func closestBySteps(p point, wire1Path, wire2Path map[point]int64) int64 {
+	return wire1Path[p] + wire2Path[p]
 }
 
 // getPath creates a set of the points the wire moves across.
