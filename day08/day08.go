7ceadfa21fc5e7f47d23aae9601972cb9ac54636
Bob Follek <bfollek@gmail.com>
2020-04-28 05:31:31 -0400

    BF WIP

--- day08/day08.go
+++ day08/day08.go
@@ -10,18 +10,8 @@ const (
 	transparent
 )
 
-type layer [][]int // layer[width][height]
-type img [][][]int // img[layers][width][height]
-
-// type imgRow []int
-
-// type layer []imgRow
-
-// type img struct {
-// 	width  int
-// 	height int
-// 	layers []layer
-// }
+type layer [][]int // [width][height]
+type img []layer
 
 // Part1 - find the layer that contains the fewest 0 digits. On that layer,
 // what is the number of 1 digits multiplied by the number of 2 digits?
