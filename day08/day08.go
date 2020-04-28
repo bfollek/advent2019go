9b415e138326aa79ccc6fe70a111f8aabfa28e10
Bob Follek <bfollek@gmail.com>
2020-04-28 05:28:54 -0400

    BF WIP

--- day08/day08.go
+++ day08/day08.go
@@ -4,18 +4,24 @@ import (
 	"github.com/bfollek/aoc19go/util"
 )
 
-type imgRow []int
+const (
+	black = iota // 0
+	white
+	transparent
+)
 
-type layer []imgRow
+type layer [][]int // layer[width][height]
+type img [][][]int // img[layers][width][height]
 
-type img struct {
-	width  int
-	height int
-	layers []layer
-}
+// type imgRow []int
 
-const width = 25
-const height = 6
+// type layer []imgRow
+
+// type img struct {
+// 	width  int
+// 	height int
+// 	layers []layer
+// }
 
 // Part1 - find the layer that contains the fewest 0 digits. On that layer,
 // what is the number of 1 digits multiplied by the number of 2 digits?
@@ -23,39 +29,64 @@ func Part1(width, height int, fileName string) int {
 	img := loadImg(width, height, fileName)
 	minSoFar := width*height + 1
 	minIndex := -1
-	for i, layer := range img.layers {
-		if nd := numDigitsInLayer(layer, 0); nd < minSoFar {
+	for i, layer := range img {
+		if nd := numPixelsInLayer(layer, black); nd < minSoFar {
 			minSoFar = nd
 			minIndex = i
 		}
 	}
-	minLayer := img.layers[minIndex]
-	return numDigitsInLayer(minLayer, 1) * numDigitsInLayer(minLayer, 2)
+	minLayer := img[minIndex]
+	return numPixelsInLayer(minLayer, white) * numPixelsInLayer(minLayer, transparent)
+}
+
+// Part2 - What message is produced after decoding your image?
+func Part2(width, height int, fileName string) int {
+	// img := loadImg(width, height, fileName)
+	// decoded := layer{}
+	// for i := 0; i < width; i++ {
+	// 	for j := 0; j < height; j++ {
+	// 		layer = append(layer, findFirstNonTransparent(i, j, img))
+	// 	}
+	// }
+	return 99
 }
 
 func loadImg(width, height int, fileName string) img {
-	img := img{width, height, []layer{}}
 	pixels := util.MustLoadIntSlice(fileName, "")
-	rows := []imgRow{}
-	for i := 0; i < len(pixels); i += width {
-		row := pixels[i : i+width]
-		rows = append(rows, row)
+	numLayers := len(pixels) / (width * height)
+	// Allocate the 3D slice
+	img := make(img, numLayers)
+	for i := range img {
+		img[i] = make([][]int, width)
+		for j := range img[i] {
+			img[i][j] = make([]int, height)
+		}
 	}
-	for i := 0; i < len(rows); i += height {
-		layer := rows[i : i+height]
-		img.layers = append(img.layers, layer)
+	// Load the pixels into it
+	p := 0
+	for i := range img {
+		for j := range img[i] {
+			for k := range img[i][j] {
+				img[i][j][k] = pixels[p]
+				p++
+			}
+		}
 	}
 	return img
 }
 
-func numDigitsInLayer(layer layer, target int) int {
+func numPixelsInLayer(layer [][]int, targetPixel int) int {
 	total := 0
 	for _, row := range layer {
-		for _, i := range row {
-			if i == target {
+		for _, pixel := range row {
+			if pixel == targetPixel {
 				total++
 			}
 		}
 	}
 	return total
+}
+
+func findFirstNonTransparent(widthPos, heightPos int, img img) int {
+	return white
 }
